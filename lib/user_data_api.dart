7a1b400c7b8bbdbdd2d5b8dcc5f92cff41b68e2c
adarkwahkathryn2019@gmail.com <adarkwahkathryn2019@gmail.com>
2021-09-17 17:43:11 +0000

    final project

---
+++ lib/user_data_api.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/p1.jpg",
+    "userPost": "images/p4.jpg",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/p2.jpg",
+    "userPost": "images/3.jpg",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/p2.jpg",
+    "userPost": "images/3.jpg",
+    "userPostLikes": "23",
+  }
+];
